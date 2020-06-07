63ccf45c3beb6d456bd231d20b135793caa15c2e
Maradonna <marco.jendryczko@gmx.net>
2020-06-07 19:32:18 +0200

    first generation attempt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 import tkinter.filedialog as fdlg
 import json
 import tkinter.ttk as ttk
+import random
 #TODO: map Nat and 2nd Nat to random image
 #TODO: store UID mapping to image in JSON in .config => avoid double assigning
 #TODO: create profiles to have assignments per savegame and switch active profiles
@@ -17,9 +18,6 @@ class NEWGAN_Manager(tk.Frame):
         self.pack()
         os.makedirs(".config", exist_ok=True)
         self.cfg_path = ".config/cfg.json"
-        if not os.path.isfile(self.cfg_path):
-            print("No config!")
-            self._write_config(self.cfg_path, {"No Profile" : True})
         self.config = self._load_config(self.cfg_path)
         self.create_widgets()
 
@@ -29,8 +27,8 @@ class NEWGAN_Manager(tk.Frame):
         #TODO: TOP Profiles
         frame_prf = tk.Frame(panel)
         frame_prf_sel = tk.Frame(panel)
-        self.combo_prf = ttk.Combobox(frame_prf_sel, values=list(self.config.keys()), state='readonly')
-        self.combo_prf.current(list(self.config.values()).index(True))
+        self.combo_prf = ttk.Combobox(frame_prf_sel, values=list(self.config["Profile"].keys()), state='readonly')
+        self.combo_prf.current(list(self.config["Profile"].values()).index(True))
         self.combo_prf.bind("<<ComboboxSelected>>", self._set_profile_status)
         ent_prf = tk.Entry(frame_prf, width=20)
         btn_prf = tk.Button(frame_prf, text='Create', command=lambda e=ent_prf, c=self.combo_prf: self._create_profile(e, c))
@@ -46,7 +44,7 @@ class NEWGAN_Manager(tk.Frame):
         frame_rtf = tk.Frame(panel)
         lbl_rtf = tk.Label(frame_rtf, width=20, text='RTF File')
         ent_rtf = tk.Entry(frame_rtf, width=40)
-        btn_rtf = tk.Button(frame_rtf, text='Browse...', command=lambda t='file', e=ent_rtf: self._file_dialog(t, e))
+        btn_rtf = tk.Button(frame_rtf, text='...', command=lambda t='file', e=ent_rtf: self._file_dialog(t, e))
         lbl_rtf.pack(side=LEFT)
         ent_rtf.pack(side=LEFT, expand=Y, fill=X)
         btn_rtf.pack(side=LEFT, padx=5)
@@ -55,14 +53,21 @@ class NEWGAN_Manager(tk.Frame):
         frame_img = tk.Frame(panel)
         lbl_img = tk.Label(frame_img, width=20, text='Newgen Image Folder')
         ent_img = tk.Entry(frame_img, width=40)
-        btn_img = tk.Button(frame_img, text='Browse...', command=lambda t='dir', e=ent_img: self._file_dialog(t, e))
+        btn_img = tk.Button(frame_img, text='...', command=lambda t='dir', e=ent_img: self._file_dialog(t, e))
         lbl_img.pack(side=LEFT)
         ent_img.pack(side=LEFT, expand=Y, fill=X)
         btn_img.pack(side=LEFT, padx=5)
         frame_img.pack(fill=X, padx='1c', pady=3)
 
         #TODO: BOTTOM: Generation
-        
+        frame_gen = tk.Frame(panel)
+        #TODO Generate Button
+        btn_gen = tk.Button(frame_gen, text='Replace Faces', command=lambda r=ent_rtf.get(), i=ent_img.get(), p=self.combo.get(): 
+                            self._replace_faces(r, i, p))
+        btn_gen.pack(side=BOTTOM)
+        #TODO Progressbar
+        frame_img.pack(fill=X, padx='1c', pady=3)
+
     def _file_dialog(self, type, ent):
         # triggered when the user clicks a 'Browse' button
         fn = None
@@ -90,6 +95,43 @@ class NEWGAN_Manager(tk.Frame):
             print(fn)
             ent.delete(0, END)
             ent.insert(END, fn)
+    def _replace_faces(self, rtf, img_dir, profile):
+        #TODO parse rtf
+        rtf_data = self.parse_rtf(rtf)
+        with open(".config/config_template", "r") as fp:
+            config_template = fp.read()
+        #TODO: walk all img subdirs and get all filenames. Create map
+        all_ethnicies = ["East European", "Scandinavian", "Mediterranean", "Arabian",
+                         "African", "South East Asian", "East Asian", "Central Asian", "UK",
+                         "Carribean"]
+        all_images = []
+        for eth in all_ethnicities.keys():
+            [all_images.append(f.name) for f in os.scandir(eth+"/") if f.is_file()]
+        prf_cfg = self._load_config(".config/"+profile+".json")
+        prf_imgs = set(prf_cfg["imgs"])
+        #TODO map rtf_data to ethnicities
+        xml_string = []
+        for player in rtf_data:
+            if player[2]:
+                p_ethnic = self.config["Ethnics"][player[2]]
+            else:
+                p_ethnic = self.config["Ethnics"][player[1]]
+            #TODO: choose picture
+            eth_imgs = set([all_images.append(f.name) for f in os.scandir(p_ethnic+"/") if f.is_file()])
+            selection_pool = eth_imgs - prf_imgs
+            player_img = random.choice(tuple(selection_pool))
+            prf_imgs.add(player_img)
+
+        #TODO create config file entry
+            xml_string.append("<record from=\"{}\" to=\"graphics/pictures/person/{}/portrait\"/>".format(player_img, player[0]))
+        #save profile metadata (used pics and config.xml)
+        xml_players = "\n".join(xml_string)
+        xml_config = config_template.replace("[players]", xml_players)
+        with open("config.xml", 'w') as fp:
+            fp.write(xml_config)
+        prf_cfg["imgs"] = list(prf_imgs)
+        self._write_config(".config/"+profile+".json", prf_cfg)
+
     def _load_config(self, path):
         with open(path, 'r') as fp:
             data = json.load(fp)
@@ -100,22 +142,25 @@ class NEWGAN_Manager(tk.Frame):
             json.dump(data, fp)
 
     def _set_profile_status(self, event):
+        #TODO: when profile switch, switch xml files
         name = self.combo_prf.get()
-        for prf, status in self.config.items():
+        for prf, status in self.config["Profile"].items():
             print(status)
             if status:
-                self.config[prf] = False
-        self.config[name] = True
+                self.config["Profile"][prf] = False
+        self.config["Profile"][name] = True
         self._write_config(self.cfg_path, self.config)
 
     def _refresh_combo(self, combo):
-        combo['values'] =list(self.config.keys())
-        combo.current(list(self.config.values()).index(True))
+        combo['values'] =list(self.config["Profile"].keys())
+        combo.current(list(self.config["Profile"].values()).index(True))
 
 
     def _create_profile(self, ent, c):
-        self.config[ent.get()] = False
+        name = ent.get()
+        self.config["Profile"][name] = False
         self._write_config(self.cfg_path, self.config)
+        self._write_config(".config/"+name+".json", {"imgs" : []})
         ent.delete(0, 'end')
         self._refresh_combo(c)
 
@@ -125,9 +170,11 @@ class NEWGAN_Manager(tk.Frame):
         if prf == "No Profile":
             print("Can't delte no profile")
             return
-        del self.config[prf]
-        self.config["No Profile"] = True
+        del self.config["Profile"][prf]
+        self.config["Profile"]["No Profile"] = True
         self._write_config(self.cfg_path, self.config)
+        os.remove(".config/"+prf+".json")
+        os.remove(".config/"+prf+".xml")
         self._refresh_combo(self.combo_prf)
 
     def parse_rtf(self, path):
@@ -149,14 +196,3 @@ class NEWGAN_Manager(tk.Frame):
 root = tk.Tk()
 app = NEWGAN_Manager(master=root)
 app.mainloop()
-
-
-
-
-#def main():
-#    parse_rtf("newgens-Willem.rtf")
-
-
-
-#if __name__ == "__main__":
-#    main()
