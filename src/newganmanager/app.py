5daf75a47f2b4c604a711eac8bd9902b073d456d
Marco <marco.jendryczko@gmx.net>
2020-11-24 13:38:03 +0100

    fix encoding when on windows
    force UTF-8 encoding

--- src/newganmanager/app.py
+++ src/newganmanager/app.py
@@ -280,11 +280,8 @@ class NewGANManager(toga.App):
         self.logger.info("Parse rtf file: {}".format(path))
         UID_regex = re.compile('([0-9]){10}')
         result_data = []
-        try:
-            rtf = open(path, 'r')
-            self.logger.info(rtf)
-        except:
-            self.logger.info("Reading RTF failed!")
+        rtf = open(path, 'r', encoding="UTF-8")
+        self.logger.info(rtf)
         rtf_data = []
         for line in rtf:
             self.logger.info(line.strip())
