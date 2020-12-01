a45a1bdac49708a1dc3bd64a27c6d1cb77b0c51c
Maradonna <marco.jendryczko@gmx.net>
2020-12-01 19:14:47 +0100

    #22: shows error

--- src/newganmanager/app.py
+++ src/newganmanager/app.py
@@ -283,6 +283,14 @@ class NewGANManager(toga.App):
         img_dir = self.profile_manager.prf_cfg['img_dir']
         profile = self.profile_manager.cur_prf
         mode = self.genmde_lst.value
+        if !os.path.isfile(rtf):
+            self._throw_error("The RTF file doesn't exist!")
+            self.gen_prg.stop()
+            return
+        if !os.path.isdir(img_dir):
+            self._throw_error("The image directory doesn't exist!")
+            self.gen_prg.stop()
+            return
         self.logger.info("rtf: {}".format(rtf))
         self.logger.info("img_dir: {}".format(img_dir))
         self.logger.info("profile: {}".format(profile))
